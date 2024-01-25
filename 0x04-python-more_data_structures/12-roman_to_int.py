1ec3151bcb79c7d62135c8bb780272c74982b748
hakeem-hassan <hlanre@ymail.com>
2024-01-25 17:53:39 -0500

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,18 +13,3 @@ def roman_to_int(roman_string: str):
             rep -= numbers[i]
 
     return rep
-
-
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
