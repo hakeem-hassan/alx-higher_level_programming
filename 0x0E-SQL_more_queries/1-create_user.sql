e8addf8230c435428eb7c70740e8e462da33425a
hakeem.hassan <hlanre@ymail.com>
2024-05-22 15:15:29 -0700

    privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- script that creates the MySQL server user user_0d_1
+-- Grant all Privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
